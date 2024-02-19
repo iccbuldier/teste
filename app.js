a9c4dd4b5b44b6cf8e96ae064c4b9ab41d28e59f
Israel Colácio <israelcolacio@outlook.com>
2024-02-19 12:11:38 -0400

    teste

---
+++ app.js
@@ -0,0 +1,10 @@
+function verificarNumero(numero) {
+    if (numero > 0) {
+        alert ('Ó número é positivo');
+    } else if (numero < 0) {
+        alert ('Ó número é negativo');
+    } else () {
+        alert ('Ó número é igual a zero');
+}
+}
+alert verificarNumero ()
